90d3d134fcd0adb919be6ff3df25d9cd3bc84bcf
Yuta Kiyama <albero.montagna@gmail.com>
2021-06-16 19:12:27 +0900

    FactoryMethodを使ってテストコードから2つのサブクラスの存在を隠した

--- src/Money.ts
+++ src/Money.ts
@@ -1,5 +1,8 @@
-export class Money {
+export abstract class Money {
   protected amount!: number;
+  abstract times(multiplier: number): Money;
+
+  constructor() {}
 
   equals(object: Money) {
     const money = object as Money;
@@ -9,5 +12,35 @@ export class Money {
       // 書籍だとJavaのgetClassを使って評価
       this.constructor.name === money.constructor.name
     );
+  }
+
+  static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
+
+  static franc(amount: number): Franc {
+    return new Franc(amount);
+  }
+}
+
+export class Franc extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Franc {
+    return new Franc(this.amount * multiplier);
+  }
+}
+
+export class Dollar extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
   }
 }
--- src/test/Money.test.ts
+++ src/test/Money.test.ts
@@ -1,29 +1,28 @@
-import { Dollar } from '../Dollar';
-import { Franc } from '../Franc';
+import { Money, Dollar, Franc } from '../Money';
 
 describe('Money', () => {
   test('掛け算のテスト', () => {
-    const five = new Dollar(5);
-    expect(five.times(2)).toEqual(new Dollar(10));
-    expect(five.times(3)).toEqual(new Dollar(15));
+    const five: Money = Money.dollar(5);
+    expect(five.times(2)).toEqual(Money.dollar(10));
+    expect(five.times(3)).toEqual(Money.dollar(15));
   });
 
   test('等価性比較のテスト', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     // 三角測量
-    expect(new Dollar(5).equals(new Dollar(6))).not.toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(6))).not.toBeTruthy();
 
     // Franc
-    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
+    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
 
     // 比較
-    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
+    expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
   });
 
   test('フランの掛け算テスト', () => {
-    const five = new Franc(5);
-    expect(five.times(2)).toEqual(new Franc(10));
-    expect(five.times(3)).toEqual(new Franc(15));
+    const five = Money.franc(5);
+    expect(five.times(2)).toEqual(Money.franc(10));
+    expect(five.times(3)).toEqual(Money.franc(15));
   });
 });
